e9c43b141a4a52fe1ac7b0ef703976925535f98e
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-11-20 23:05:42 +0900

    Omit DB name

--- index.ts
+++ index.ts
@@ -5,13 +5,13 @@ import { createServer } from 'http'
 import { randomBytes } from 'crypto'
 import cors from 'cors'
 
-const DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017'
+const DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/draw'
 const PORT = ((p) => (p != null ? parseInt(p) : 8000))(process.env.PORT)
 ;(async () => {
   const mongoClient = await MongoClient.connect(DATABASE_URL, {
     useUnifiedTopology: true
   })
-  const db = mongoClient.db('draw')
+  const db = mongoClient.db()
   const picturesCollection = db.collection('pictures')
   picturesCollection.createIndex('id', { unique: true })
 
