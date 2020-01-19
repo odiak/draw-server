99aec9c07652fbe765e78264ed11b2859deacdf4
Kaido Iwamoto <h6824.kaido@gmail.com>
2020-01-19 11:06:20 +0900

    Set ID for existing paths

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import SocketIO from 'socket.io'
 import { createServer } from 'http'
-import { randomBytes } from 'crypto'
 import cors from 'cors'
 import { pictureToSVG } from './src/pictureToSVG'
 import { connectToMongo } from './src/connectToMongo'
+import { Collection } from 'mongodb'
+import { generateId } from './src/generateId'
 
 const PORT = ((p) => (p != null ? parseInt(p) : 8000))(process.env.PORT)
 ;(async () => {
@@ -13,8 +14,13 @@ const PORT = ((p) => (p != null ? parseInt(p) : 8000))(process.env.PORT)
   const app = express()
   app.use(cors({ origin: '*' }))
 
+  setTimeout(() => {
+    setIdForPaths(picturesCollection)
+  })
+
   app.get('/pictures/:pictureId', async (req, res) => {
     const p = await picturesCollection.findOne({ id: req.params.pictureId })
+    await setIdForPath(picturesCollection, p)
     res.json(p)
   })
 
@@ -38,7 +44,7 @@ const PORT = ((p) => (p != null ? parseInt(p) : 8000))(process.env.PORT)
   io.on('connection', (socket) => {
     socket.on('savePicture', async ({ id, title, paths }, cb) => {
       if (id == null) {
-        id = randomBytes(16).toString('hex')
+        id = generateId()
         const now = new Date()
         await picturesCollection.insertOne({ id, title, paths, createdAt: now, updatedAt: now })
       } else {
@@ -50,3 +56,30 @@ const PORT = ((p) => (p != null ? parseInt(p) : 8000))(process.env.PORT)
 
   server.listen(PORT)
 })()
+
+async function setIdForPaths(picturesCollection: Collection) {
+  const cur = await picturesCollection.find()
+  while (cur.hasNext()) {
+    const picture = await cur.next()
+    await setIdForPath(picturesCollection, picture)
+  }
+}
+
+async function setIdForPath(picturesCollection: Collection, picture: any): Promise<any> {
+  let changed = false
+  const newPaths = picture.paths.map((path: any) => {
+    if (path.id != null) return path
+
+    changed = true
+    return {
+      ...path,
+      id: generateId()
+    }
+  })
+
+  if (!changed) return picture
+
+  const newPicture = { ...picture, paths: newPaths }
+  picturesCollection.updateOne({ id: picture.id }, newPicture)
+  return newPicture
+}
