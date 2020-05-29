429099442d7ae0ce712621f48887b7c6e6fcd7f8
Kaido Iwamoto <h6824.kaido@gmail.com>
2020-05-29 22:51:31 +0900

    Refactor

--- src/pathsToSvg.ts
+++ src/pathsToSvg.ts
@@ -1,28 +1,9 @@
 import escapeHTML from 'escape-html'
 import { Path } from './commonTypes'
+import { measure } from './measure'
 
 export function pathsToSVG(paths: Array<Path>): string {
-  let minX = Number.POSITIVE_INFINITY
-  let maxX = Number.NEGATIVE_INFINITY
-  let minY = Number.POSITIVE_INFINITY
-  let maxY = Number.NEGATIVE_INFINITY
-  for (const path of paths) {
-    for (const { x, y } of path.points) {
-      minX = Math.min(minX, x)
-      maxX = Math.max(maxX, x)
-      minY = Math.min(minY, y)
-      maxY = Math.max(maxY, y)
-    }
-  }
-  if (!Number.isFinite(minX)) minX = 0
-  if (!Number.isFinite(maxX)) maxX = 0
-  if (!Number.isFinite(minY)) minY = 0
-  if (!Number.isFinite(maxY)) maxY = 0
-  const offset = 20
-  const baseX = minX - offset
-  const baseY = minY - offset
-  const width = maxX - minX + offset * 2
-  const height = maxY - minY + offset * 2
+  const { width, height, baseX, baseY } = measure(paths)
 
   const pathStrs = paths.map(({ points, width, color }: any) => {
     const desc = points
