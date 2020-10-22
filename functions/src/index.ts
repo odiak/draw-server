7c64255be866c51647ba506e4e9e06b62b82de74
Kaido Iwamoto <h6824.kaido@gmail.com>
2020-10-23 00:00:41 +0900

    Add code for Firebase Functions to migrate data

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions'
+import * as firebase from 'firebase-admin'
+
+const defaultRegion = 'asia-northeast1'
+
+export const migrateData = functions.region(defaultRegion).https.onCall(
+  async ({ token }: { token: string }, context): Promise<void> => {
+    const db = firebase.firestore()
+
+    const { uid } = context.auth!
+    const ds = await db
+      .collection('migrationTokens')
+      .doc(token)
+      .get()
+    const oldUid = ds.data()!.uid
+
+    const qs = await db
+      .collection('pictures')
+      .where('ownerId', '==', oldUid)
+      .get()
+
+    const bulkWriter = db.bulkWriter()
+
+    for (const d of qs.docs) {
+      bulkWriter.update(d.ref, { ownerId: uid })
+    }
+
+    await bulkWriter.close()
+
+    await ds.ref.delete()
+  }
+)
